1492cffd49a19bd845e041591ce1b204acf2ea29
YuKai <v955160v@gmail.com>
2024-10-23 21:35:49 +0800

    test.js return

--- test/githubAction.test.js
+++ test/githubAction.test.js
@@ -1,27 +1,29 @@
-// const request = require('supertest');
-// const express = require('express');
+// npm test
 
-// const app = require('../githubAction'); // 引用你的Express應用程式
+const request = require('supertest');
+const express = require('express');
 
-// describe('GET /', () => {
-//   it('should return GitHub Action', (done) => {
-//     request(app)
-//       .get('/')
-//       .expect(200)  // 預期狀態碼為200
-//       .expect('GitHub Action', done);  // 預期返回 'GitHub Action'
-//   });
-// });
+const app = require('../githubAction'); // 引用你的Express應用程式
 
-// describe('POST /git', () => {
-//   it('should return an object with name, attack, and defense', (done) => {
-//     request(app)
-//       .post('/git')
-//       .expect(200)
-//       .expect(res => {
-//         res.body.name === 'Joe';
-//         res.body.attack === 50;
-//         res.body.defense === 30;
-//       })
-//       .end(done);
-//   });
-// });
+describe('GET /', () => {
+  it('should return GitHub Action', (done) => {
+    request(app)
+      .get('/')
+      .expect(200)  // 預期狀態碼為200
+      .expect('GitHub Action', done);  // 預期返回 'GitHub Action'
+  });
+});
+
+describe('POST /git', () => {
+  it('should return an object with name, attack, and defense', (done) => {
+    request(app)
+      .post('/git')
+      .expect(200)
+      .expect(res => {
+        res.body.name === 'Joe';
+        res.body.attack === 50;
+        res.body.defense === 30;
+      })
+      .end(done);
+  });
+});

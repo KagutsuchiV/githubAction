9d7a0e35e89b68e77bb1f837cccfd40dbf6ffd2b
YuKai <v955160v@gmail.com>
2024-10-23 18:39:42 +0800

    test github_action

--- githubAction.js
+++ githubAction.js
@@ -6,6 +6,14 @@ app.get('/', (req,res)=>{
     res.send('GitHub Action');
 });
 
+app.post('/git',(req,res)=>{
+    const data = {
+        name: 'Joe',
+        attack: 50,
+        denfense: 30
+    };
+});
+
 app.listen(port,()=>{
     console.log(`http://localhost:${port}`);
 });

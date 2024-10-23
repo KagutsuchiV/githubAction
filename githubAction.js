d04992cb1e8641121ee068a1fe78b3cd9c94614e
YuKai <v955160v@gmail.com>
2024-10-23 19:00:47 +0800

    test action new

--- githubAction.js
+++ githubAction.js
@@ -1,19 +1,25 @@
 const express = require('express');
-const port = 3000;
 const app = express();
+const port = 3000;
 
-app.get('/', (req,res)=>{
+app.get('/', (req, res) => {
     res.send('GitHub Action');
 });
 
-app.post('/git',(req,res)=>{
+app.post('/git', (req, res) => {
     const data = {
         name: 'Joe',
         attack: 50,
-        denfense: 30
+        defense: 30
     };
+    res.json(data);  // 返回 JSON
 });
 
-app.listen(port,()=>{
-    console.log(`http://localhost:${port}`);
-});
+// 如果此應用程式是被測試引用的，則不啟動伺服器
+if (require.main === module) {
+    app.listen(port, () => {
+        console.log(`http://localhost:${port}`);
+    });
+}
+
+module.exports = app; // 將app導出以供測試使用
---
+++ test/githubAction.test.js
@@ -0,0 +1,27 @@
+const request = require('supertest');
+const express = require('express');
+
+const app = require('../githubAction'); // 引用你的Express應用程式
+
+describe('GET /', () => {
+  it('should return GitHub Action', (done) => {
+    request(app)
+      .get('/')
+      .expect(200)  // 預期狀態碼為200
+      .expect('GitHub Action', done);  // 預期返回 'GitHub Action'
+  });
+});
+
+describe('POST /git', () => {
+  it('should return an object with name, attack, and defense', (done) => {
+    request(app)
+      .post('/git')
+      .expect(200)
+      .expect(res => {
+        res.body.name === 'Joe';
+        res.body.attack === 50;
+        res.body.defense === 30;
+      })
+      .end(done);
+  });
+});
